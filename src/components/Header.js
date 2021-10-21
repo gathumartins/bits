39e574aee4075600e275941a4df3542cfe27c4f3
gathumartins <gathumartins@gmail.com>
2021-10-21 19:36:27 +0300

    Navbar change on scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Helmet } from 'react-helmet';
 // import { Container } from 'react-bootstrap'
 import * as headerStyles from './Header.module.css';
 
 function Header() {
+    const [myHeader, setMyHeader] = useState(false);
+    const changemyHeading = () => {
+        if (window.scrollY >= 80) {
+            setMyHeader(true)
+        } else {
+            setMyHeader(false)
+        }
+    }
+    window.addEventListener('scroll', changemyHeading);
     return (
-        <section className={`navbar fixed-top ${headerStyles.myHeader}`}>
-            <header className="flex shadow-md">
+        <section className={`navbar  shadow-md fixed-top ${myHeader ? `${headerStyles.myHeader}, ${headerStyles.scroll}` : `${headerStyles.myHeader}`}`}>
+            <header className="flex">
                 <div className={`${headerStyles.navBrand} bg-white absolute`}>
                     <img src="./imgs/logo.png" alt="Nairobits Top Logo" fluid="true" />
                 </div>
