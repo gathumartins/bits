a44709449f56b44f9a07ecc000f2ca72e2decfe3
gathumartins <gathumartins@gmail.com>
2021-10-21 15:21:09 +0300

    Second commit

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -6,7 +6,7 @@ function Video() {
     return (
         <Container className="py-15">
             <Row className={`d-flex align-items-center ${vStyles.vCont}`}>
-                <Col xs={12} sm={12} md={12} lg={6} className="relative">
+                <Col xs={12} sm={12} md={12} lg={6} className="relative mb-10">
                     <div className={`${vStyles.leftCont} py-10`}>
                         <div className="block">
                             <h2>NairoBits is</h2>
