25462db20835a54917b6822d428e1d9f275e0bfc
gathumartins <gathumartins@gmail.com>
2021-10-22 20:35:54 +0300

    fixed sidebar width

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -3,7 +3,7 @@ import { FaLinkedin, FaFacebookSquare, FaTwitter, FaInstagramSquare } from 'reac
 
 function Sidebar() {
     return (
-        <div className={`d-none d-sm-none d-md-block p-5 relative bg-white sidebar`}>
+        <div className={`d-none d-sm-none d-md-block py-5 relative bg-white sidebar`}>
             <ul className="space-y-8 absolute h-[300px] bottom-20 socialLinks">
                 <li><a href="/" target="_blank"><span className="hidden">linkedin</span> <FaLinkedin className="h-7 w-7" /></a></li>
                 <li><a href="/" target="_blank"><span className="hidden">faceboook</span> <FaFacebookSquare className="h-7 w-7" /></a></li>
