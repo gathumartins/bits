50352f4a28686c812cf9429bcdbf60d0e26b1f64
gathumartins <gathumartins@gmail.com>
2021-10-23 14:09:01 +0300

    bug fixing on deploy

--- src/pages/whoweare.js
+++ src/pages/whoweare.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import WhoWeAreBan from '../components/WhoWeAre/WhoWeAreBan';
 import CoreValues from '../components/WhoWeAre/CoreValues';
 import OpsModel from '../components/WhoWeAre/OpsModel';
 
 function WhoWeAre() {
     return (
         <Layout>
-            <WhoWeAreBan />
             <CoreValues />
             <OpsModel />
         </Layout>
