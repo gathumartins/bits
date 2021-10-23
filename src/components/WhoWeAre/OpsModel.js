128b932d73819644a24873b0a103570c32c00e3a
gathumartins <gathumartins@gmail.com>
2021-10-23 05:34:15 +0300

    Who we are page update

---
+++ src/components/WhoWeAre/OpsModel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import * as opsStyles from './about.module.css';
+
+function OpsModel() {
+    return (
+        <section className={opsStyles.opsOuter}>
+            <Container fluid={`md`}>
+                <Row>
+                    <Col xs={12} sm={12} md={3} lg={2} className="my-3">
+                        <div className={opsStyles.opsTitle}>
+                            <h3>Explore NairoBits operations and programming</h3>
+                        </div>
+                    </Col>
+
+                    <Col xs={12} sm={12} md={9} lg={10}>
+                        <Row>
+                            <Col xs={6} sm={6} md={6} lg={3} className={`${opsStyles.opOuter} my-3`}>
+                                <div>
+                                    <h4>Our History</h4>
+                                    <div className={opsStyles.underOps}></div>
+                                </div>
+                            </Col>
+                            <Col xs={6} sm={6} md={6} lg={3} className={`${opsStyles.opOuter} my-3`}>
+                                <div>
+                                    <h4>Our Operating Model</h4>
+                                    <div className={opsStyles.underOps}></div>
+                                </div>
+                            </Col>
+                            <Col xs={6} sm={6} md={6} lg={3} className={`${opsStyles.opOuter} my-3`}>
+                                <div>
+                                    <h4>Our Team</h4>
+                                    <div className={opsStyles.underOps}></div>
+                                </div>
+                            </Col>
+                            <Col xs={6} sm={6} md={6} lg={3} className={`${opsStyles.opOuter} my-3`}>
+                                <div>
+                                    <h4>Our Partners</h4>
+                                    <div className={opsStyles.underOps}></div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </section >
+    )
+}
+
+export default OpsModel
